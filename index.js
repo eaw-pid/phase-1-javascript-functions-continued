e3224caaf11a71301c0c1eeef7a93110f30e0156
Erin Wise <erinwise88@gmail.com>
2023-08-07 17:42:14 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity1 = "go to the office") {
+    return `This Monday, I will ${activity1}.`
+}
+
+function wrapAdjective(style = "*") {
+    return function innerFunction(adj1 = "special") {
+        return `You are ${style}${adj1}${style}!`
+    }
+}
